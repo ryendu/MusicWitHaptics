34c21b14369c417b5d9adc8a8ce6281b9369117f
Ryan Du <ryandu9221@gmail.com>
2021-07-16 23:02:25 -0700

    Initial Commit

---
+++ MusicWitHaptics/MusicWitHapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicWitHapticsApp.swift
+//  MusicWitHaptics
+//
+//  Created by Ryan D on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicWitHapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
