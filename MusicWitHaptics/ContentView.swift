d8f752b9de54b1c2dfa6277f0e405014fac2d3d0
Ryan Du <ryandu9221@gmail.com>
2021-07-17 08:58:15 -0700

    initial commit

--- MusicWitHaptics/ContentView.swift
+++ MusicWitHaptics/ContentView.swift
@@ -6,16 +6,44 @@
 //
 
 import SwiftUI
+import MobileCoreServices
+import AVFoundation
+import CoreHaptics
 
 struct ContentView: View {
+    @State var showPicker = false
+    @State var url: URL? = nil
+    @State var engine: CHHapticEngine!
+    @State var player: AVAudioPlayer?
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Button(action: {
+                self.playTestHaptic()
+            }, label: {
+                Text("Play test haptic")
+            })
+        }.onAppear {
+            let audioSession = AVAudioSession.sharedInstance()
+            
+            engine = try? CHHapticEngine(audioSession: audioSession)
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    func playTestHaptic(){
+        do {
+            
+            let path = Bundle.main.path(forResource: "In My Blood", ofType:"mp3")!
+            let url = URL(fileURLWithPath: path)
+            player = try AVAudioPlayer(contentsOf: url)
+            
+            
+            guard let path = Bundle.main.path(forResource: "imb", ofType: "ahap") else { return }
+            try engine?.start()
+            
+            try engine?.playPattern(from: URL(fileURLWithPath: path))
+            player!.play()
+            
+        } catch {
+            print("SOMETHING WENT WRONG: \(error)")
+        }
     }
 }
