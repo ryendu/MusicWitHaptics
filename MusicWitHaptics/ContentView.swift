ad29821da2dff7cd7002bbeb4a5ad82f705f4734
Ryan Du <ryandu9221@gmail.com>
2021-07-17 14:46:34 -0700

    Updated playback

--- MusicWitHaptics/ContentView.swift
+++ MusicWitHaptics/ContentView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 import MobileCoreServices
 import AVFoundation
 import CoreHaptics
+import MediaPlayer
 
 struct ContentView: View {
     @State var showPicker = false
     @State var url: URL? = nil
     @State var engine: CHHapticEngine!
     @State var player: AVAudioPlayer?
+    @State var advancedHapticPlayer: CHHapticAdvancedPatternPlayer?
+    @State var songs = ["In My Blood", "Heat Waves"]
+    @State var isPlaying = false
     var body: some View {
         NavigationView{
             VStack{
-                Text("Listen with headphones for best effect!").padding()
+                Text("Listen with headphones for best effect! Note: Haptics require app to be open and in focus.").padding()
                 Button(action: {
                     self.playHapticSong(name: "In My Blood")
                 }, label: {
@@ -30,27 +34,85 @@ struct ContentView: View {
                     SongCardView(songName: "Heat Waves", albumPhotoURL: URL(string: "https://i.scdn.co/image/ab67616d00001e02712701c5e263efc8726b1464")!, artistName: "Glass Animals")
                 }).buttonStyle(PlainButtonStyle())
                 Spacer()
-            }.onAppear {
-                let audioSession = AVAudioSession.sharedInstance()
+                VStack{
+                    if !self.isPlaying{
+                        Button(action: {
+                            if self.player == nil{
+                                self.playHapticSong(name: songs[0])
+                            } else {
+                                self.player?.play()
+                                try? self.advancedHapticPlayer?.resume(atTime: 0)
+                                self.isPlaying = true
+                            }
+                        }, label: {
+                            Image(systemName: "play.circle.fill").resizable().frame(width: 50, height: 50)
+                        })
+                    } else {
+                        Button(action: {
+                            if self.player != nil{
+                                self.player?.pause()
+                                try? self.advancedHapticPlayer?.pause(atTime: 0)
+                                self.isPlaying = false
+                            }
+                        }, label: {
+                            Image(systemName: "pause.circle.fill").resizable().frame(width: 50, height: 50)
+                        })
+                    }
+                }.padding()
                 
-                engine = try? CHHapticEngine(audioSession: audioSession)
+            }.onAppear {
+                do {
+                    try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                    try AVAudioSession.sharedInstance().setActive(true)
+                    
+                    engine = try CHHapticEngine(audioSession: AVAudioSession.sharedInstance())
+                    try engine.start()
+                } catch {
+                    print("ERRORRRRRRRRRRRRRRR")
+                    print(error)
+                }
+                
             }
             .navigationBarTitle(Text("Music With Haptics"))
         }
     }
+    func createPatternFromAHAP(_ filename: String) -> CHHapticPattern? {
+        // Get the URL for the pattern in the app bundle.
+        let patternURL = Bundle.main.url(forResource: filename, withExtension: "ahap")!
+        
+        do {
+            // Read JSON data from the URL.
+            let patternJSONData = try Data(contentsOf: patternURL, options: [])
+            
+            // Create a dictionary from the JSON data.
+            let dict = try JSONSerialization.jsonObject(with: patternJSONData, options: [])
+            
+            if let patternDict = dict as? [CHHapticPattern.Key: Any] {
+                // Create a pattern from the dictionary.
+                return try CHHapticPattern(dictionary: patternDict)
+            }
+        } catch let error {
+            print("Error creating haptic pattern: \(error)")
+        }
+        return nil
+    }
+    
     func playHapticSong(name: String){
         do {
             
             let path = Bundle.main.path(forResource: name, ofType:"mp3")!
             let url = URL(fileURLWithPath: path)
+            
             player = try AVAudioPlayer(contentsOf: url)
+//            player?.prepareToPlay()
+            
+            guard let ahapPattern = createPatternFromAHAP(name) else { return }
+            self.advancedHapticPlayer = try self.engine.makeAdvancedPlayer(with:ahapPattern)
             
             
-            guard let path = Bundle.main.path(forResource: name, ofType: "ahap") else { return }
-            try engine?.start()
-            
-            try engine?.playPattern(from: URL(fileURLWithPath: path))
-            player!.play()
+            try self.advancedHapticPlayer?.start(atTime: 0)
+            player?.play()
+            self.isPlaying = true
             
         } catch {
             print("SOMETHING WENT WRONG: \(error)")
