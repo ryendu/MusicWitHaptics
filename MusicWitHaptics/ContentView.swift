9c0b0aac464b9ac436ef3c925d33fd5a57dcb10e
Ryan Du <ryandu9221@gmail.com>
2021-07-17 12:18:17 -0700

    added heat waves and new ui

--- MusicWitHaptics/ContentView.swift
+++ MusicWitHaptics/ContentView.swift
@@ -16,27 +16,37 @@ struct ContentView: View {
     @State var engine: CHHapticEngine!
     @State var player: AVAudioPlayer?
     var body: some View {
-        VStack{
-            Button(action: {
-                self.playTestHaptic()
-            }, label: {
-                Text("Play test haptic")
-            })
-        }.onAppear {
-            let audioSession = AVAudioSession.sharedInstance()
-            
-            engine = try? CHHapticEngine(audioSession: audioSession)
+        NavigationView{
+            VStack{
+                Text("Listen with headphones for best effect!").padding()
+                Button(action: {
+                    self.playHapticSong(name: "In My Blood")
+                }, label: {
+                    SongCardView(songName: "In My Blood", albumPhotoURL: URL(string: "https://i.scdn.co/image/ab67616d00001e02269423eb6467e308c0fbce24")!, artistName: "Shawn Mendes")
+                }).buttonStyle(PlainButtonStyle())
+                Button(action: {
+                    self.playHapticSong(name: "Heat Waves")
+                }, label: {
+                    SongCardView(songName: "Heat Waves", albumPhotoURL: URL(string: "https://i.scdn.co/image/ab67616d00001e02712701c5e263efc8726b1464")!, artistName: "Glass Animals")
+                }).buttonStyle(PlainButtonStyle())
+                Spacer()
+            }.onAppear {
+                let audioSession = AVAudioSession.sharedInstance()
+                
+                engine = try? CHHapticEngine(audioSession: audioSession)
+            }
+            .navigationBarTitle(Text("Music With Haptics"))
         }
     }
-    func playTestHaptic(){
+    func playHapticSong(name: String){
         do {
             
-            let path = Bundle.main.path(forResource: "In My Blood", ofType:"mp3")!
+            let path = Bundle.main.path(forResource: name, ofType:"mp3")!
             let url = URL(fileURLWithPath: path)
             player = try AVAudioPlayer(contentsOf: url)
             
             
-            guard let path = Bundle.main.path(forResource: "imb", ofType: "ahap") else { return }
+            guard let path = Bundle.main.path(forResource: name, ofType: "ahap") else { return }
             try engine?.start()
             
             try engine?.playPattern(from: URL(fileURLWithPath: path))
---
+++ MusicWitHaptics/SongCardView.swift
@@ -0,0 +1,42 @@
+//
+//  SongCardView.swift
+//  MusicWitHaptics
+//
+//  Created by Ryan D on 7/17/21.
+//
+
+import SwiftUI
+import URLImage
+
+struct SongCardView: View {
+    var songName: String
+    var albumPhotoURL: URL
+    var artistName: String
+    var body: some View {
+        HStack{
+            URLImage(self.albumPhotoURL,content: {img in
+                img
+                    .resizable()
+                    .frame(width:100,height:100)
+                    .padding(.trailing)
+            })
+            VStack(alignment: .leading){
+                Text(songName)
+                    .font(.headline)
+                Text(artistName)
+                    .font(.caption)
+            }
+            Spacer()
+        }
+        .padding(.vertical)
+        .padding(.horizontal)
+        .background(RoundedRectangle(cornerRadius: 5).foregroundColor(.white).shadow(radius: 8))
+        .padding(.horizontal)
+    }
+}
+
+struct SongCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        SongCardView(songName: "In My Blood", albumPhotoURL: URL(string: "https://i.scdn.co/image/ab67616d00001e02269423eb6467e308c0fbce24")!, artistName: "Shawn Mendes")
+    }
+}
